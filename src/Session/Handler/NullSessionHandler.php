8647e62dbba0c15657cb3dd74ef3dfa12a20ec2e
exirizo <60464978+exirizo@users.noreply.github.com>
2020-01-31 13:34:01 -0500

    Add file doc headers.

--- src/Session/Handler/NullSessionHandler.php
+++ src/Session/Handler/NullSessionHandler.php
@@ -1,4 +1,9 @@
 <?php declare(strict_types=1);
+/**
+ * @see       <https://github.com/kooser-framework/framework> for the canonical source repository
+ * @copyright <https://github.com/kooser-framework/framework/blob/master/copyright> Framework Copyright
+ * @license   <https://github.com/kooser-framework/framework/blob/master/license> New BSD License
+ */
 
 namespace Kooser\Session\Handler;
 
