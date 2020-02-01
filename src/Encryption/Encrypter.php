b2cc9e14358d49044110c4540b900a1837d0c1cb
exirizo <60464978+exirizo@users.noreply.github.com>
2020-02-01 10:46:04 -0500

    Add encrypter interface.

---
+++ src/Encryption/Encrypter.php
@@ -0,0 +1,15 @@
+<?php declare(strict_types=1);
+/**
+ * @see       <https://github.com/kooser-framework/framework> for the canonical source repository
+ * @copyright <https://github.com/kooser-framework/framework/blob/master/copyright> Framework Copyright
+ * @license   <https://github.com/kooser-framework/framework/blob/master/license> New BSD License
+ */
+
+namespace Kooser\Encryption;
+
+/**
+ * The encrypter interface.
+ */
+interface Encrypter
+{
+}
