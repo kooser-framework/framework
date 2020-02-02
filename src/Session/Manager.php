e895f2992e0a69ad45c0ad2202d02616cfe3cfd2
exirizo <60464978+exirizo@users.noreply.github.com>
2020-02-02 03:50:26 -0500

    Add the session manager interface.

---
+++ src/Session/Manager.php
@@ -0,0 +1,15 @@
+<?php declare(strict_types=1);
+/**
+ * @see       <https://github.com/kooser-framework/framework> for the canonical source repository
+ * @copyright <https://github.com/kooser-framework/framework/blob/master/copyright> Framework Copyright
+ * @license   <https://github.com/kooser-framework/framework/blob/master/license> MIT License
+ */
+
+namespace Kooser\Session;
+
+/**
+ * The session manager interface.
+ */
+interface Manager
+{
+}
