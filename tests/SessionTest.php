8c28d89fc1bf02bcb1d74af260aad958b11787b5
exirizo <60464978+exirizo@users.noreply.github.com>
2020-02-05 22:03:46 -0500

    Add logic.

---
+++ tests/SessionTest.php
@@ -0,0 +1,20 @@
+<?php declare(strict_types=1);
+/**
+ * @see       <https://github.com/kooser-framework/framework> for the canonical source repository
+ * @copyright <https://github.com/kooser-framework/framework/blob/master/copyright> Framework Copyright
+ * @license   <https://github.com/kooser-framework/framework/blob/master/license> MIT License
+ */
+
+namespace Kooser\Test\Session;
+
+use PHPUnit\Framework\TestCase;
+
+class SessionManagerTest extends TestCase
+{
+    /**
+     * @runInSeparateProcess
+     */
+    public function testSessions()
+    {
+    }
+}
