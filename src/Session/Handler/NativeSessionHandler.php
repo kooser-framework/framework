a8676eebf20c1259b426515d2ced651eb9d0fbd1
exirizo <60464978+exirizo@users.noreply.github.com>
2020-02-07 16:54:43 -0500

    Add the native session handler.

---
+++ src/Session/Handler/NativeSessionHandler.php
@@ -0,0 +1,18 @@
+<?php declare(strict_types=1);
+/**
+ * @see       <https://github.com/kooser-framework/framework> for the canonical source repository
+ * @copyright <https://github.com/kooser-framework/framework/blob/master/copyright> Framework Copyright
+ * @license   <https://github.com/kooser-framework/framework/blob/master/license> New BSD License
+ */
+
+namespace Kooser\Session;
+
+use SessionHandler;
+use SessionHandlerInterface;
+
+/**
+ * The native session handler.
+ */
+class NativeSessionHandler extends SessionHandler implements SessionHandlerInterface
+{
+}
