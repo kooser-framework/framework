30c8a0416b62acfcfc659efd64b22e72cf2513a8
exirizo <60464978+exirizo@users.noreply.github.com>
2020-02-07 15:31:38 -0500

    Add the hasher logic.

--- src/Hashing/Hasher.php
+++ src/Hashing/Hasher.php
@@ -12,4 +12,49 @@ namespace Kooser\Hashing;
  */
 interface Hasher
 {
+    /**
+     * Construct a new argon2id hasher.
+     *
+     * @param array $options The argon2id hasher options.
+     *
+     * @return void Returns nothing.
+     */
+    public function __construct(array $options = []);
+
+    /**
+     * Set the hasher options.
+     *
+     * @param array $options The hasher options.
+     *
+     * @return \Kooser\Hashing\Hasher Returns the hasher.
+     */
+    public function setOptions(array $options = []): Hasher;
+
+    /**
+     * Verify the password matches the given hash.
+     *
+     * @param string $password The password to check.
+     * @param string $hash     The hash the password must match.
+     *
+     * @return bool Returns true if the password matches and false if not.
+     */
+    public function verify(string $password, string $hash): bool;
+
+    /**
+     * Comput a new hash.
+     *
+     * @param string $password The password to hash.
+     *
+     * @return string Returns the hashed password.
+     */
+    public function compute(string $password): ?string;
+
+    /**
+     * Determine if the hash needs a rehash.
+     *
+     * @param string $hash The hash to check.
+     *
+     * @return bool Returns true if the hash needs a rehash and false if not.
+     */
+    public function needsRehash(string $hash): bool;
 }
